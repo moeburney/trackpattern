0a08369bc056575392deb27656595211b5b54dae
moe <moe@e6080b8f-847c-49b8-8a92-75ecbcdee227>
2011-06-03 20:49:22 +0000

    Creating branch of tracklist

---
+++ branch/home/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+#from django.contrib import admin
+#admin.autodiscover()
+
+
+urlpatterns = patterns('tracklist.home.views',
+    (r'^$', 'home'),
+    (r'^settings/$', 'settings'),                                              
+    (r'^search/$', 'search'),                       
+)
