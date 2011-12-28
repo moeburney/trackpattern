38a150643ec06f33cea3a322ce81436cfd87e506
Rohan Kanade <github@rohankanade.com>
2011-12-28 14:22:14 +0530

    reports 1,2

--- settings.py
+++ settings.py
@@ -120,3 +120,43 @@ INSTALLED_APPS = (
     'django.contrib.admin'
     )
 SESSION_COOKIE_DOMAIN = 'trackpattern.com'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+        },
+    'handlers': {
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/mylog.log',
+            'maxBytes': 1024*1024*20, # 5 MB
+            'backupCount': 10,
+            'formatter':'standard',
+            },
+        'request_handler': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/django_request.log',
+            'maxBytes': 1024*1024*5, # 5 MB
+            'backupCount': 5,
+            'formatter':'standard',
+            },
+        },
+    'loggers': {
+
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.request': { # Stop SQL debug from logging to main logger
+                            'handlers': ['request_handler'],
+                            'level': 'DEBUG',
+                            'propagate': False
+        },
+        }
+}
