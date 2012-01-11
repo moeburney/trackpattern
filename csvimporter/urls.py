232aef66f87d35b2ce3d7512ea1422667c2c26a7
Rohan Kanade <github@rohankanade.com>
2012-01-11 23:00:09 +0530

    redeploy changes

--- csvimporter/urls.py
+++ csvimporter/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from core.models import Customer, Product, Sale
 
-urlpatterns = patterns('tracklist.csvimporter.views',
+urlpatterns = patterns('csvimporter.views',
     url(r'^customer/$', 'new', kwargs={'model': Customer}, name='new-csv'),
     url(r'^product/$', 'new', kwargs={'model': Product}, name='new-csv'),
     url(r'^sale/$', 'new', kwargs={'model': Sale}, name='new-csv'),
