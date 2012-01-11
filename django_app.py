9d982d195c636ef5806a178731186532ddf249a6
Rohan Kanade <github@rohankanade.com>
2012-01-11 19:00:01 +0530

    redeploy changes

--- django_app.py
+++ django_app.py
@@ -1,14 +1,14 @@
 __author__ = 'rohan'
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 path = '/root/tracklist'
 if path not in sys.path:
     sys.path.append('/root')
     sys.path.append(path)
 
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 import django.core.handlers.wsgi
 
