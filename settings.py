74f2e8ad2b32f486af5db09e81f4614aa223ee26
Rohan Kanade <github@rohankanade.com>
2012-01-11 19:19:33 +0530

    redeploy changes

--- settings.py
+++ settings.py
@@ -120,56 +120,56 @@ INSTALLED_APPS = (
     'django.contrib.admin'
     )
 SESSION_COOKIE_DOMAIN = 'trackpattern.com'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-        },
-    'handlers': {
-        'default': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/root/tracklist-logs/logs/default.log',
-            'maxBytes': 1024*1024*20, # 5 MB
-            'backupCount': 10,
-            'formatter':'standard',
-            },
-        'request_handler': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/root/tracklist-logs/logs/django_request.log',
-            'maxBytes': 1024*1024*5, # 5 MB
-            'backupCount': 5,
-            'formatter':'standard',
-            },
-        'app': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/root/tracklist-logs/logs/app.log',
-            'maxBytes': 1024*1024*5, # 5 MB
-            'backupCount': 5,
-            'formatter':'standard',
-            }
-        },
-    'loggers': {
-
-        '': {
-            'handlers': ['default'],
-            'level': 'DEBUG',
-            'propagate': True
-        },
-        'applog': {
-            'handlers': ['app'],
-            'level': 'DEBUG',
-            'propagate': True
-        },
-        'django.request': { # Stop SQL debug from logging to main logger
-                            'handlers': ['request_handler'],
-                            'level': 'DEBUG',
-                            'propagate': False
-        },
-        }
-}
+#LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': True,
+#    'formatters': {
+#        'standard': {
+#            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+#        },
+#        },
+#    'handlers': {
+#        'default': {
+#            'level':'DEBUG',
+#            'class':'logging.handlers.RotatingFileHandler',
+#            'filename': '/root/tracklist-logs/logs/default.log',
+#            'maxBytes': 1024*1024*20, # 5 MB
+#            'backupCount': 10,
+#            'formatter':'standard',
+#            },
+#        'request_handler': {
+#            'level':'DEBUG',
+#            'class':'logging.handlers.RotatingFileHandler',
+#            'filename': '/root/tracklist-logs/logs/django_request.log',
+#            'maxBytes': 1024*1024*5, # 5 MB
+#            'backupCount': 5,
+#            'formatter':'standard',
+#            },
+#        'app': {
+#            'level':'DEBUG',
+#            'class':'logging.handlers.RotatingFileHandler',
+#            'filename': '/root/tracklist-logs/logs/app.log',
+#            'maxBytes': 1024*1024*5, # 5 MB
+#            'backupCount': 5,
+#            'formatter':'standard',
+#            }
+#        },
+#    'loggers': {
+#
+#        '': {
+#            'handlers': ['default'],
+#            'level': 'DEBUG',
+#            'propagate': True
+#        },
+#        'applog': {
+#            'handlers': ['app'],
+#            'level': 'DEBUG',
+#            'propagate': True
+#        },
+#        'django.request': { # Stop SQL debug from logging to main logger
+#                            'handlers': ['request_handler'],
+#                            'level': 'DEBUG',
+#                            'propagate': False
+#        },
+#        }
+#}
