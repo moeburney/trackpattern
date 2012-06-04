c26ae506b65acf8b4c30bd4d4345c939ba48c032
Rohan Kanade <github@rohankanade.com>
2012-06-04 12:10:48 +0530

    dont goto chargify.

--- home/views.py
+++ home/views.py
@@ -567,7 +567,7 @@ def signup(request):
 #                    user.id, user.first_name, user.last_name, user.email))
 
             login_user = authenticate(username=user.username, password=form.cleaned_data['password1'])
-            tlogin(request, login_user)
+            tlogin(request)
             return redirect('/home/')
 
     else:
